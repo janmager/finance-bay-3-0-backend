7e8895a1b8b5be6e81e3a4ed1151a4214c8b6603
Jan Mager <jasiu.mager@gmail.com>
2025-07-13 17:52:12 +0200

    cron jobs

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js"
 import cors from 'cors';
+import job from './config/cron.js'
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ const app = express();
 app.use(cors())
 app.use(rateLimiter)
 app.use(express.json());
+
+if(process.env.NODE_ENV === 'production') job.start();
 
 const PORT = process.env.PORT || 5001;
 

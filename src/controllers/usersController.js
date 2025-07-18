51e8f249d5c3ba9c4a38969f9f7679866f11ffe8
Jan Mager <jasiu.mager@gmail.com>
2025-07-19 00:18:01 +0200

    fix

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -4,13 +4,14 @@ export async function createUser(req, res){
     try{
         const { user_id, email, avatar } = req.body;
     
-        if(!user_id || !email){
+        if(!user_id){
             return res.status(400).json({message: "All fields are required."})
         }
 
-        const username = email.split('@')[0];
-
-        console.log(username)
+        let username = '' 
+        if(email) {
+            username = email.split('@')[0];
+        }
 
         const if_is = await sql`
             SELECT * FROM users WHERE id = ${user_id} OR email = ${email}
@@ -21,13 +22,13 @@ export async function createUser(req, res){
 
         const users = await sql`
         INSERT INTO users (id, email, username, monthly_limit, avatar) 
-        VALUES (${user_id}, ${email}, ${username}, 3000, ${avatar})
+        VALUES (${user_id}, ${email}, ${username ?? ''}, 3000, ${avatar})
             RETURNING *
         `;
         res.status(201).json({data: users[0]})
     }   
     catch(e){
-        console.log("Error creating user: ",);
+        console.log("Error creating user: ", e);
         res.status(500).json({message: "Something went wrong."});
     } 
 }

f09ca8f3dbcc36ca7e87a509637f8593229272e9
Jan Mager <jasiu.mager@gmail.com>
2025-07-30 17:24:49 +0200

    recurring auto checking system done

--- src/config/cron.js
+++ src/config/cron.js
@@ -1,40 +1,21 @@
 import cron from "cron";
 import https from "https";
-import { checkAllUsersForRecurrings } from "../controllers/recurringsController";
+import { checkAllUsersForRecurrings } from "../controllers/recurringsController.js";
+import { API_URL } from "./db.js";
 
 // for active state render server (going to sleep after 15min of disactive)
 export const wakeupJob = new cron.CronJob("*/14 * * * *", function () {
   https
-    .get(process.env.API_URL, (res) => {
-      if (res.statusCode === 200) console.log("[CRON] wakeupJob GET request sent successfully");
+    .get(API_URL, (res) => {
+      if (res.statusCode === 200) console.log("[CRON] wakeupJob successfully.");
       else console.log("GET request failed", res.statusCode);
     })
     .on("error", (e) => console.error("Error while sending request", e));
 });
 
-export const checkUsersRecurrings = new cron.CronJob("*/2 * * * *", function async () {
-  console.log(process.env.API_URL+'/api/crons/check-all-recurrings-for-users')
+// At 0 minutes past the hour, every 6 hours
+export const checkUsersRecurrings = new cron.CronJob("0 0 */6 * * *", function async () {
+  console.log(API_URL+'/api/crons/check-all-recurrings-for-users')
   checkAllUsersForRecurrings();
+  console.log("[CRON] checkUsersRecurrings successfully.");
 });
-
-
-
-
-
-
-
-// CRON JOB EXPLANATION:
-// Cron jobs are scheduled tasks that run periodically at fixed intervals
-// we want to send 1 GET request for every 14 minutes
-
-// How to define a "Schedule"?
-// You define a schedule using a cron expression, which consists of 5 fields representing:
-
-//! MINUTE, HOUR, DAY OF THE MONTH, MONTH, DAY OF THE WEEK
-
-//? EXAMPLES && EXPLANATION:
-//* 14 * * * * - Every 14 minutes
-//* 0 0 * * 0 - At midnight on every Sunday
-//* 30 3 15 * * - At 3:30 AM, on the 15th of every month
-//* 0 0 1 1 * - At midnight, on January 1st
-//* 0 * * * * - Every hour

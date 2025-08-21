24b94b8315e1849391f752011960ee4a7478d037
Jan Mager <jasiu.mager@gmail.com>
2025-08-21 12:23:14 +0200

    better ai quest

--- src/controllers/aiController.js
+++ src/controllers/aiController.js
@@ -81,10 +81,10 @@ Odpowiedź chcę otrzymać w formacie JSON z następującą strukturą:
   "percent_created_at": 90
 }
 
-Dla parametru created_at jeśli masz dane (odczytane z grafiki) to podaj datę w formacie timestamp.
+Dla parametru created_at jeśli masz dane to podaj datę zakupu z odczytaną z grafiki w formacie timestamp.
 Dla parametru category masz dostępne kategorie: food, shopping, transportation, entertainment, bills, health, house, clothes, car, education, gifts, animals, recurring, travel, overdue, incoming-payments, other.
-Dla parametru title jeśli masz dane to podaj nazwę sklepu.
-Dla parametru description jeśli masz dane to podaj nazwy kupionych produktów (ich główne nazwy bez szczegółów), wypisane w liście.
+Dla parametru title jeśli masz dane to podaj nazwę sklepu, samą jego oficjalną nazwę bez rozszerzeń.
+Dla parametru description jeśli masz dane to podaj nazwy kupionych produktów (ich główne nazwy bez szczegółów), wypisane w liście, nie uwzględniaj rabatów, upustów itp.
 Parametry pewności (pewnosc_*) to wartości procentowe (0-100) które zawierają informacje jak bardzo jesteś pewny/zgodny co do poprawności zaczytania wartości z grafiki. Jeśli nie jesteś czegoś pewien/nie masz takich danych ustaw wartość parametru na null, a wartości procentowe na 0.`;
     }
 
